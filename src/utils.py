277ca70d122e05a8a62fa0cbd91f0036900425a5
Rishabh S <rishabhs2114@gmail.com>
2026-02-28 18:17:34 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 # convert this object into pickle file at file location
 def save_object(file_path,obj):
@@ -19,12 +19,20 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys)      
 #reports of all the models
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train) #training the model
+
+
             y_train_pred = model.predict(X_train) #predicting the train data
             y_test_pred = model.predict(X_test) #predicting the test data
             train_model_score = r2_score(y_train,y_train_pred) #calculating r2 score for train data
